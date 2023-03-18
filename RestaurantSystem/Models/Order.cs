ca48222c44992831b900478b43a45b30b9064312
unknown <alexanderdev@yahoo.com>
2023-03-18 13:14:06 +0200

    Update haha

--- RestaurantSystem/Models/Order.cs
+++ RestaurantSystem/Models/Order.cs
@@ -8,21 +8,6 @@ namespace RestaurantSystem.Data.Model
 {
     public class Order
     {
-        public Order(int id,int tableid, int productid, int quantity)
-        {
-            this.TableId = tableid;
-            this.Id = id;
-            this.ProductId = productid;
-            this.Quantity = quantity;
-        }
-
-        public Order() { }
-
-        public int Id { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
-        public int TableId { get; set; }
-        public Table Table { get; set; }
-        public ICollection<Product> Products { get; set; }
+        
     }
 }
--- RestaurantSystem/Models/Product.cs
+++ RestaurantSystem/Models/Product.cs
@@ -10,19 +10,6 @@ namespace RestaurantSystem.Data.Model
 {
     public class Product
     {
-        public Product(int id, string name,int orderid, decimal price)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.OrderId = orderid;
-            this.Price = price;
-        }
-        public Product() { }
-
-        public int Id { get; set; }
-        public int OrderId { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public Order Order { get; set; }
+        
     }
 }
